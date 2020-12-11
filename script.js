89fae90a87e2a6f3b63d7f617b45775590139e53
Nash <72932477+Nashwalt@users.noreply.github.com>
2020-12-11 01:13:14 -0500

    Added function to colour code time blocks.

--- script.js
+++ script.js
@@ -9,4 +9,30 @@ function displayTime() {
     setTimeout(displayTime, 1000);
 }
 displayTime();
+
+//Function to colour code time blocks.
+function hourUpdater() {
+    var  currentHour = moment().format('H')
+    $('.colour').each(function() {
+    var blockTime = parseInt($(this).prop('id'));
+    if (blockTime == currentHour) {
+        $(this).addClass("present");
+    //  $(this).removeClass("future");
+    //  $(this).removeClass("past");
+    } else if (blockTime < currentHour){
+        $(this).addClass("past");
+    //  $(this).removeClass("present");
+    //  $(this).removeClass("future");
+    }else {
+        $(this).addClass("future");
+    //  $(this).removeClass("present");
+    //  $(this).removeClass("past");
+    }
+});
+}
+hourUpdater();
+var checkTime = setInterval(hourUpdater, 15000);
+
+
+
 })
