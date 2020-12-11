25e7ed4d843bbf60ea884e7ffaa3f679a98693b6
Nash <72932477+Nashwalt@users.noreply.github.com>
2020-12-10 23:39:25 -0500

    Added function to display local time.

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 //Have everything on page when loaded.
 $(document).ready(function() {
 $("#currentDay").text(moment().format("LL")); 
+
+//Function to display clock.
+function displayTime() {
+    var time = moment().format('hh:mm A');
+    $('#clock').html(time);
+    setTimeout(displayTime, 1000);
+}
+displayTime();
 })
