6a4479ad6faa3d11af309b96009ee06d1226c6d2
Nash <72932477+Nashwalt@users.noreply.github.com>
2020-12-12 00:25:10 -0500

    Added function to clear local storage each day.

--- script.js
+++ script.js
@@ -1,65 +1,71 @@
 //Have everything on page when loaded.
-$("#currentDay").text(moment().format("dddd")); 
-$("#currentDay").append("<p class=date></p>"); 
-$(".date").text(moment().format("LL")); 
-$(".date").append("<p id=clock></p>"); 
-$("p").css("line-height","150%");
-
-//Function to display clock.
-function displayTime() {
-  var time = moment().format('hh:mm:ss A');
-  $('#clock').html(time);
-  setTimeout(displayTime, 1000);
-}
-displayTime();
-
-//Function to colour code time blocks.
-function hourUpdater() {
-  var  currentHour = moment().format('H')
-  $('.colour').each(function() {
-  var blockTime = parseInt($(this).prop('id'));
-  if (blockTime == currentHour) {
-      $(this).addClass("present");
-  //  $(this).removeClass("future");
-  //  $(this).removeClass("past");
-  } else if (blockTime < currentHour){
-      $(this).addClass("past");
-  //  $(this).removeClass("present");
-  //  $(this).removeClass("future");
-  }else {
-      $(this).addClass("future");
-  //  $(this).removeClass("present");
-  //  $(this).removeClass("past");
+$(document).ready(function() {
+  $("#currentDay").text(moment().format("dddd")); 
+  $("#currentDay").append("<p class=date></p>"); 
+  $(".date").text(moment().format("LL")); 
+  $(".date").append("<p id=clock></p>"); 
+  $("p").css({"line-height":"150%", "color":"blue"}); 
+  
+  //Function to display clock.
+  function displayTime() {
+      var time = moment().format('hh:mm:ss A');
+      $('#clock').html(time);
+      setTimeout(displayTime, 1000);
   }
-});
-}
-  hourUpdater();
-  setInterval(hourUpdater, 90000);
-   
-
-//Save information entered.
-var events = [];                                                    //Saving events as an array.
-
-$(".saveBtn").on("click", function() {                              //Event listener for the save button.
-    var task = $(this).siblings(".description").val();
-    var timeslot = $(this).parent().attr("id");
-    events.push({timeslot:timeslot, task:task})
-    localStorage.setItem("events", JSON.stringify(events));         //Saving events array into local storage.
-    
+  displayTime();
+  
+  //Function to colour code time blocks.
+  function hourUpdater() {
+      var  currentHour = moment().format('H')
+      $('.colour').each(function() {
+      var blockTime = parseInt($(this).prop('id'));
+      if (blockTime == currentHour) {
+          $(this).addClass("present");
+      //  $(this).removeClass("future");
+      //  $(this).removeClass("past");
+      } else if (blockTime < currentHour){
+          $(this).addClass("past");
+      //  $(this).removeClass("present");
+      //  $(this).removeClass("future");
+      }else {
+          $(this).addClass("future");
+      //  $(this).removeClass("present");
+      //  $(this).removeClass("past");
+      }
   });
-
-//Display Events after saved to local storage.
-var storedEvents = JSON.parse(localStorage.getItem("events"));
-
-  if (storedEvents !== null) {
-    events = storedEvents;
   }
-
-  for(var i = 0; i < events.length; i++) {
-    var userDescription = events[i].task;
-    $("#" + events[i].timeslot).children(".description").text(userDescription);
+      hourUpdater();
+      setInterval(hourUpdater, 90000);
+       
+  //Save information entered.
+  var events = [];                                                    //Saving events as an array.
+  
+  $(".saveBtn").on("click", function() {                              //Event listener for the save button.
+      var task = $(this).siblings(".description").val();
+      var timeslot = $(this).parent().attr("id");
+      events.push({timeslot:timeslot, task:task})
+      localStorage.setItem("events", JSON.stringify(events));         //Saving events array into local storage.
+      
+    });
+  
+  //Display Events after saved to local storage.
+  var storedEvents = JSON.parse(localStorage.getItem("events"));
+  
+    if (storedEvents !== null) {
+      events = storedEvents;
+    }
+  
+    for(var i = 0; i < events.length; i++) {
+      var userDescription = events[i].task;
+      $("#" + events[i].timeslot).children(".description").text(userDescription);
+    }
+  
+  //Function to clear local storage each day.
+  function clearTask() {
+    var newDay = moment().format('H')
+    if (newDay === 0){
+    localStorage.clear(); 
+    }
   }
-
-
-
-})
+    clearTask();
+  })
